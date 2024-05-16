3701de5c2ffe022b1b244a68992e2228153df189
Andreas Jonathan Imm. <jojonathanimmanuel@gmail.com>
2024-05-16 16:27:50 +0700

    Pull Scene Game Script Complete
    - Players and Bots pull with delay and ropeSpeed
    - Bots have difficulty value to measure how fast do they pull
    - Music and sound effects used placeholders.. please add

--- Assets/Scripts/PullGame.cs
+++ Assets/Scripts/PullGame.cs
@@ -7,23 +7,58 @@ public class PullGame : MonoBehaviour
 {
     private bool winYet = false;
     private bool controlsEnabled = true;
-    public float power = 10f;
-    public float delay = 0.25f;
+    public float delay = 0.1f;
+    public float ropeMoveSpeed = 0.1f;
+    public int difficulty = 500;
 
     // Players and bots
     public GameObject[] players;
     public GameObject[] bots;
+    public GameObject rope;
     public BoxCollider border;
     
     // Sounds
     public AudioClip winSound;
     public AudioClip loseSound;
     public AudioClip pullSound;
-    public AudioClip music;
+    public AudioClip endSound;
     public AudioSource audioSource;
+    public GameObject musicPlayer;
 
     // UI
     public TMP_Text winText;
+
+    private void OnWin() {
+        if (winYet) return;
+        winYet = true;
+        winText.text = "You Win!";
+        winText.gameObject.SetActive(true);
+        audioSource.PlayOneShot(winSound);
+        musicPlayer.GetComponent<AudioSource>().PlayOneShot(endSound);
+        musicPlayer.GetComponent<AudioSource>().Stop();
+    }
+
+    private void OnLose() {
+        // Ensuring no spam triggers
+        if (winYet) return;
+        winYet = true;
+        winText.text = "You Lose!";
+        winText.gameObject.SetActive(true);
+        audioSource.PlayOneShot(loseSound);
+        musicPlayer.GetComponent<AudioSource>().PlayOneShot(endSound);
+        musicPlayer.GetComponent<AudioSource>().Stop();
+    }
+
+    private void BotMove() {
+        // Move the rope and players to the bots side
+        rope.transform.position += new Vector3(0, 0, ropeMoveSpeed);
+        foreach(GameObject player in players) {
+            player.transform.position += new Vector3(0, 0, ropeMoveSpeed);
+        }
+        foreach(GameObject bot in bots) {
+            bot.transform.position += new Vector3(0, 0, ropeMoveSpeed);
+        }
+    }
 
     private IEnumerator controlDelay()
     {
@@ -32,16 +67,33 @@ public class PullGame : MonoBehaviour
         controlsEnabled = true;
     }
 
-    private void OnButtonPress() {
+    public void OnButtonPress() {
         if (controlsEnabled && !winYet) {
+            rope.transform.position -= new Vector3(0, 0, ropeMoveSpeed);
+            audioSource.PlayOneShot(pullSound);
             foreach (GameObject player in players) {
-                player.GetComponent<Rigidbody>().AddForce(Vector3.left * power, ForceMode.Impulse);
-                audioSource.PlayOneShot(pullSound);
+                player.transform.position -= new Vector3(0, 0, ropeMoveSpeed);
             }
             foreach (GameObject bot in bots) {
-                bot.GetComponent<Rigidbody>().AddForce(Vector3.right * power, ForceMode.Impulse);
+                bot.transform.position -= new Vector3(0, 0, ropeMoveSpeed);
             }
             StartCoroutine(controlDelay());
+        }
+    }
+
+    private void Update() {
+        if (!winYet) {
+            // Bot pulls randomly
+            int random = Random.Range(0, difficulty);
+            if (random < 5) {
+                BotMove();
+            }
+            if (border.bounds.Contains(players[0].transform.position)) {
+                OnLose();
+            }
+            else if (border.bounds.Contains(bots[0].transform.position)) {
+                OnWin();
+            }
         }
     }
 }
